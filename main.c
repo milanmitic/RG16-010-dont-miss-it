96febcab71ea932d5da4b162cddcd08cab0e15e7
Aleksandar Muljaic <m.aleksandar1994@gmail.com>
2016-12-05 10:14:05 +0100

    Crtaju se aktivne linije i pojavljuju se nove, kao i linije sa objektima. Registruje se pritiskanje vise tastera istovremeno.

--- main.c
+++ main.c
@@ -1,26 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <GL/glut.h>
+#include <time.h>
 
-#define TIMER_ID 0
-#define TIME_INTERVAL 50
+#define SUMMON_LINE_TIMER 0
+#define REFRESH_TIMER 1
+#define SUMMON_LINE_OBJECTS_TIMER 2
+#define OBJECTS_REFRESH_TIMER 3
+#define TIME_INTERVAL 1000
+#define NUMBER_OF_OBJECTS 5
 
 /* Deklaracije callback funkcija */
 static void on_keyboard(unsigned char key, int x, int y);
+static void keyboard_up(unsigned char key, int x, int y);
 static void on_reshape(int width, int hight);
 static void on_display(void);
 static void on_timer(int value);
 
+/* Deklaracije funkcija */
+void draw_line(int line_num);
+void draw_blocks(int k);
+
 /* Dimenzije objekta */
 const static float rect_x = 2.3;
-const static float rect_y = 0.1;
+const static float rect_y = 0.05;
 
-static float x_curr, y_curr;  /* Tekuce koordinate centra objekta */	
+const static float block_x = 2.3/5.0;
+const static float block_y = 0.15;
+
 static float v_x, v_y;		  /* Komponente vektora brzine kretanja objekta */
 static int animaton_ongoing;  /* Fleg: 1 - animacija je u toku, inace nije */
 
 /* Dimenzije prozora */
 static int window_width, window_height;
+
+/* struktura za liniju */
+typedef struct 
+{
+	float x_curr,y_curr;
+}line;
+
+/* struktura za liniju sa objektima */
+typedef struct 
+{
+	int keys[256];
+	float x_q, x_w, x_e, x_r, x_t, y_curr;
+}line_object;
+
+int next = 0;
+int index=0;
+line_object line_objects[NUMBER_OF_OBJECTS]; 
+line lines[NUMBER_OF_OBJECTS];
 
 int main(int argc,char* argv[])
 {
@@ -37,9 +67,8 @@ int main(int argc,char* argv[])
 	glutKeyboardFunc(on_keyboard);
 	glutReshapeFunc(on_reshape);
 	glutDisplayFunc(on_display);
+	glutKeyboardUpFunc(keyboard_up);
 
-	x_curr = 0.0;
-	y_curr = 2.0 - rect_y/2;
 	v_y = -rect_y/2;
 	animaton_ongoing=0;
 
@@ -47,10 +76,36 @@ int main(int argc,char* argv[])
 	glClearColor(0, 0, 0, 0);
 	glEnable(GL_DEPTH_TEST);
 
+	/* Postavljaju se pocetne vrednosti, 
+	 	da se ne iscrtava na traci pre pokretanja animacije */
+	int k;	
+	for(k=0;k<NUMBER_OF_OBJECTS;k++)
+	{
+		float pom = -3.0;
+		lines[k].x_curr = 0.0;
+		lines[k].y_curr = pom;
+
+		line_objects[k].x_q = pom;
+		line_objects[k].x_w = pom;
+		line_objects[k].x_e = pom;
+		line_objects[k].x_r = pom;
+		line_objects[k].x_t = pom;
+	}
+
 	/* Ulazi se u glavnu petlju */
 	glutMainLoop();
 
 	return 0;
+}
+
+static void keyboard_up(unsigned char key,int x, int y)
+{
+	line_objects[next].keys['q'] = 0;
+	line_objects[next].keys['w'] = 0;
+	line_objects[next].keys['e'] = 0;
+	line_objects[next].keys['r'] = 0;
+	line_objects[next].keys['t'] = 0;
+
 }
 
 static void on_keyboard(unsigned char key, int x, int y)
@@ -66,45 +121,97 @@ static void on_keyboard(unsigned char key, int x, int y)
 			/* Pokrece se animacija */
 			if(!animaton_ongoing)
 			{
-				glutTimerFunc(TIME_INTERVAL, on_timer, TIMER_ID);
+				glutTimerFunc(TIME_INTERVAL, on_timer, SUMMON_LINE_TIMER);
+				glutTimerFunc(10, on_timer, REFRESH_TIMER);
+
+				glutTimerFunc(2000, on_timer, SUMMON_LINE_OBJECTS_TIMER);
+				glutTimerFunc(10, on_timer, OBJECTS_REFRESH_TIMER);
+				
 				animaton_ongoing = 1;
 			}
 			break;
-		case 's':
-			/* Zaustavlja se animacija */
-			animaton_ongoing = 0;
-			break;
 		case 'q':
-			/* Takodje se zaustavlja animacija ali samo ako se pritisne u 
-			 * odgovarajucem trenutku
-			 */
-			if(y_curr <= -0.3 && y_curr >= -0.5)
-				{
-					animaton_ongoing = 0;
-					printf("q\n");
-				}
+			line_objects[next].keys['q']=1;
 			break;
+		case 'w':
+			line_objects[next].keys['w']=1;
+			break;
+		case 'e':
+			line_objects[next].keys['e']=1;
+			break;
+		case 'r':
+			line_objects[next].keys['r']=1;
+			break;
+		case 't':
+			line_objects[next].keys['t']=1;
+			break;
+
 	}
 }
 
 static void on_timer(int value)
 {
 	/* Proverava se da li callback dolazi od odgovarajuceg tajmera */
-	if(value != TIMER_ID)
-		return;
+	if(value == SUMMON_LINE_TIMER)
+	{ 
+		/* Azuriraju se koordinate centra linije */
+		lines[index].x_curr = 0.0;
+		lines[index].y_curr = 2.0 - rect_y/2;
+				
+		index= (++index == NUMBER_OF_OBJECTS) ? 0 : index;
+		
+		/* Po potrebi se ponovo postavlja tajmer */
+		if(animaton_ongoing)
+		{
+			glutTimerFunc(TIME_INTERVAL, on_timer, SUMMON_LINE_TIMER);
+		}
+	}
+	if(value == REFRESH_TIMER)
+	{	
+		int k;
+		for(k=0;k<NUMBER_OF_OBJECTS;k++)
+		{
+			lines[k].x_curr += v_x;
+			lines[k].y_curr += v_y;
+		}
+		glutPostRedisplay();
 
-	/* Azuriraju se koordinate centra objekta */
-	x_curr += v_x;
-	y_curr += v_y;
-	printf("x_curr = %.2f, y_curr = %.2f\n",x_curr,y_curr);
-
-	/* Forsira se ponovno iscrtavanje prozora */
-	glutPostRedisplay();
-
-	/* Po potrebi se ponovo postavlja tajmer */
-	if(animaton_ongoing)
+		if(animaton_ongoing)
+			{
+				glutTimerFunc(10, on_timer, REFRESH_TIMER);
+			}	
+	}
+	if(value == SUMMON_LINE_OBJECTS_TIMER)
 	{
-		glutTimerFunc(TIME_INTERVAL, on_timer, TIMER_ID);
+		line_objects[next].x_q = -1.15 + block_x/2;
+		line_objects[next].x_w = line_objects[next].x_q + block_x;
+		line_objects[next].x_e = line_objects[next].x_q + block_x*2;
+		line_objects[next].x_r = line_objects[next].x_q + block_x*3;
+		line_objects[next].x_t = line_objects[next].x_q + block_x*4;
+		line_objects[next].y_curr = 2.0 - block_y/2;
+
+		next = (++next == NUMBER_OF_OBJECTS) ? 0 : next;
+
+		if(animaton_ongoing)
+		{
+			int r = rand()/(float)RAND_MAX * (TIME_INTERVAL - 500) + 500;
+			//printf("rand:%d\n", r);
+			glutTimerFunc(r, on_timer, SUMMON_LINE_OBJECTS_TIMER);
+		}
+	}
+	if(value == OBJECTS_REFRESH_TIMER)
+	{
+		int k;
+		for(k=0;k<NUMBER_OF_OBJECTS;k++)
+		{
+			line_objects[k].y_curr += v_y;
+		}
+		glutPostRedisplay();
+
+		if(animaton_ongoing)
+		{
+			glutTimerFunc(10, on_timer, OBJECTS_REFRESH_TIMER);
+		}	
 	}
 }
 
@@ -112,6 +219,95 @@ static void on_reshape(int width, int height)
 {
 	window_width = width;
 	window_height = height;
+}
+
+/* Iscrtava se linija */
+void draw_line(int line_num)
+{
+	glColor3f(0.2,0.6,1);
+    glBegin(GL_POLYGON);
+        glVertex3f(lines[line_num].x_curr - rect_x/2,
+        			 lines[line_num].y_curr - rect_y/2, 0.11);
+        glVertex3f(lines[line_num].x_curr + rect_x/2,
+        			 lines[line_num].y_curr - rect_y/2, 0.11);
+        glVertex3f(lines[line_num].x_curr + rect_x/2, 
+        			 lines[line_num].y_curr + rect_y/2, 0.11);
+        glVertex3f(lines[line_num].x_curr - rect_x/2,
+        			 lines[line_num].y_curr + rect_y/2, 0.11);
+    glEnd();
+
+	glutPostRedisplay();
+}
+
+/* Iscrtava se linija sa objektima */
+void draw_blocks(int k)
+{
+	//prvi blok : q
+	glColor3f(1.0, 0.0, 0.0);
+	glBegin(GL_POLYGON);
+        glVertex3f(line_objects[k].x_q - block_x/2,
+        			 line_objects[k].y_curr - block_y/2, 0.12);
+        glVertex3f(line_objects[k].x_q + block_x/2,
+         			 line_objects[k].y_curr - block_y/2, 0.12);
+        glVertex3f(line_objects[k].x_q + block_x/2, 
+        			 line_objects[k].y_curr + block_y/2, 0.12);
+        glVertex3f(line_objects[k].x_q - block_x/2,
+        			 line_objects[k].y_curr + block_y/2, 0.12);
+    glEnd();
+
+    //drugi blok : w
+    glColor3f(1.0,1.0,0.0);
+	glBegin(GL_POLYGON);
+        glVertex3f(line_objects[k].x_w - block_x/2,
+        			 line_objects[k].y_curr - block_y/2, 0.12);
+        glVertex3f(line_objects[k].x_w + block_x/2,
+         			 line_objects[k].y_curr  - block_y/2, 0.12);
+        glVertex3f(line_objects[k].x_w + block_x/2, 
+        			 line_objects[k].y_curr + block_y/2, 0.12);
+        glVertex3f(line_objects[k].x_w - block_x/2,
+        			 line_objects[k].y_curr + block_y/2, 0.12);
+    glEnd();
+
+    //treci blok : e
+    glColor3f(0.0, 0.0, 1.0);
+	glBegin(GL_POLYGON);
+        glVertex3f(line_objects[k].x_e - block_x/2,
+        			 line_objects[k].y_curr - block_y/2, 0.12);
+        glVertex3f(line_objects[k].x_e + block_x/2,
+         			 line_objects[k].y_curr - block_y/2 , 0.12);
+        glVertex3f(line_objects[k].x_e + block_x/2, 
+        			 line_objects[k].y_curr + block_y/2, 0.12);
+        glVertex3f(line_objects[k].x_e - block_x/2,
+        			 line_objects[k].y_curr + block_y/2, 0.12);
+    glEnd();
+
+    //cetvrti blok : r
+    glColor3f(0.0, 1.0, 0.0);
+	glBegin(GL_POLYGON);
+        glVertex3f(line_objects[k].x_r - block_x/2,
+        			 line_objects[k].y_curr - block_y/2, 0.12);
+        glVertex3f(line_objects[k].x_r + block_x/2,
+         			 line_objects[k].y_curr - block_y/2 , 0.12);
+        glVertex3f(line_objects[k].x_r + block_x/2, 
+        			 line_objects[k].y_curr + block_y/2, 0.12);
+        glVertex3f(line_objects[k].x_r - block_x/2,
+        			 line_objects[k].y_curr + block_y/2, 0.12);
+    glEnd();
+
+    //peti blok : t
+    glColor3f(1.0, 0.5, 0.0);
+	glBegin(GL_POLYGON);
+        glVertex3f(line_objects[k].x_t - block_x/2,
+        			 line_objects[k].y_curr - block_y/2, 0.12);
+        glVertex3f(line_objects[k].x_t + block_x/2,
+         			 line_objects[k].y_curr - block_y/2 , 0.12);
+        glVertex3f(line_objects[k].x_t + block_x/2, 
+        			 line_objects[k].y_curr + block_y/2, 0.12);
+        glVertex3f(line_objects[k].x_t - block_x/2,
+        			 line_objects[k].y_curr + block_y/2, 0.12);
+    glEnd();
+
+	glutPostRedisplay();
 }
 
 static void on_display(void)
@@ -134,21 +330,28 @@ static void on_display(void)
 
 	/* Crtamo osnovnu traku */
 	glBegin(GL_QUADS);
- 	glColor3f(0.3, 1.0, 1.0);     
- 	glVertex3f(-1.2, -1.0, 0);
- 	glVertex3f(-1.2, 4.0, 0);
- 	glVertex3f(1.2, 4.0, 0);
- 	glVertex3f(1.2, -1.0, 0);
+	 	glColor3f(0.3, 1.0, 1.0);     
+	 	glVertex3f(-1.2, -1.0, 0);
+	 	glVertex3f(-1.2, 4.0, 0);
+	 	glVertex3f(1.2, 4.0, 0);
+	 	glVertex3f(1.2, -1.0, 0);
 	glEnd();
 
-	/* Crtamo objekat */
-    glColor3f(0.2,0.6,1);
-    glBegin(GL_POLYGON);
-        glVertex3f(x_curr - rect_x / 2, y_curr - rect_y / 2, 0.11);
-        glVertex3f(x_curr + rect_x / 2, y_curr - rect_y / 2, 0.11);
-        glVertex3f(x_curr + rect_x / 2, y_curr + rect_y / 2, 0.11);
-        glVertex3f(x_curr - rect_x / 2, y_curr + rect_y / 2, 0.11);
-    glEnd();
+	/* Crtaju se linije */
+	int i;
+	for(i=0;i<NUMBER_OF_OBJECTS;i++)
+	{
+    	draw_line(i);
+	}
+
+	/* Crtaju se linije sa objektima */
+	for(i=0;i<NUMBER_OF_OBJECTS;i++)
+	{
+		draw_blocks(i);
+		// printf("q: %d w: %d e: %d r: %d t: %d \n",line_objects[i].keys['q'],
+		// 	line_objects[i].keys['w'], line_objects[i].keys['e'], line_objects[i].keys['r'],
+		// 	line_objects[i].keys['t']);
+	}
 
     /* Postavlja se nova slika na prozor */
 	glutSwapBuffers();
